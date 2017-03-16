3349b55acabba718c45de40d77c49fbfc6c299f1
Rui Costa <ei12150@fe.up.pt>
2017-03-16 15:57:57 +0000

    ConfigManager

--- sharedbackup/src/backend/ConfigManager.java
+++ sharedbackup/src/backend/ConfigManager.java
@@ -1,6 +1,7 @@
 package backend;
 
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class ConfigManager {
 	private static final String VERSION = "1.0";
@@ -8,7 +9,7 @@ public class ConfigManager {
 	private MDBListener  mdrListener;
 	private MDRListener mdbListener;
 
-	private InetAddress mcAddr = null, mdbAddr = null, mdrAddr= null;
+	private InetAddress mcAddr = null , mdbAddr = null , mdrAddr = null ;
 	private int mMCport = 0, mMDBport = 0, mMDRport = 0;
 
 
@@ -20,10 +21,9 @@ public class ConfigManager {
 		mdrListener=null;
 		mdbListener= null;
 
-
 	}
 
-	public  static ConfigManager getConfigManager(){
+	public static ConfigManager getConfigManager(){
 		if(iConfigManager==null){
 			iConfigManager = new ConfigManager();
 		}
@@ -31,16 +31,16 @@ public class ConfigManager {
 	}
 
 
-	public boolean setAdresses(String mcIP,String mcPort,String mdbIP,String mdbPort,String mcrIP,String mcrPort) {
+	public boolean setAdresses(String mcIP,String mcPort,String mdbIP,String mdbPort,String mdrIP,String mdrPort) {
 
 		try {
-			this.mcAddr= InetAddress.getByName(mcAddr);
-			this.mdbAddr = InetAddress.getByName(mdbAddr);
-			this.mdrAddr = InetAddress.getByName(mdrAddr);
+			this.mcAddr = InetAddress.getByName(mcIP);
+			this.mdbAddr = InetAddress.getByName(mdbIP);
+			this.mdrAddr = InetAddress.getByName(mdrIP);
 
-			this.mMCport= int. mcPort;
-			this.mMDBport = mdbPort;
-			this.mMDRport = mdrPort;
+			this.mMCport= Integer.parseInt(mcPort);
+			this.mMDBport = Integer.parseInt(mdbPort);
+			this.mMDRport = Integer.parseInt(mdrPort);
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 			return false;
