650765b19823555f29263be769e45b757e18dea9
Rui Costa <ei12150@fe.up.pt>
2017-03-16 14:32:13 +0000

    configManager

--- sharedbackup/src/backend/configManager.java
+++ sharedbackup/src/backend/configManager.java
@@ -5,6 +5,7 @@ public class configManager {
 	public boolean openCommunication(String mcIP,String mcPort,String mdbIP,String mdbPort,String mcrIP,String mcrPort) {
 		
 		
+		
 		return true;
 	}
 	
