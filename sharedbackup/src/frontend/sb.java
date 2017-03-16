23ea4ba61489f6bc0be9de52a58a2b66845c1bd9
Rui Costa <ei12150@fe.up.pt>
2017-03-16 14:12:12 +0000

    start app

---
+++ sharedbackup/src/frontend/sb.java
@@ -0,0 +1,17 @@
+package frontend;
+
+import java.io.File;
+
+public class sb {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String fileTosave = args[1];
+		
+		openCommunication();
+		
+		putChunk(fileTosave);
+
+	}
+
+}
