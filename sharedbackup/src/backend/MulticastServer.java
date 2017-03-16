ef5b85663ad3b6d83854bafb7c7b7c06c91d760a
Rui Costa <ei12150@fe.up.pt>
2017-03-16 15:26:07 +0000

    MulticastServer

--- sharedbackup/src/backend/MulticastServer.java
+++ sharedbackup/src/backend/MulticastServer.java
@@ -4,11 +4,13 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 
 public class MulticastServer {
 
 	private static int TTL = 1;
+	private static int MAX_PACKET_SIZE  = 64000;
 
 	// Server Port for Multicast
 	public int multicastPort;
@@ -46,6 +48,54 @@ public class MulticastServer {
 			e.printStackTrace();
 
 		}
+		
+	}
+	
+	public boolean sendMessage(byte[] messg) throws Exception {
+		
+		DatagramPacket packet = new DatagramPacket(messg, messg.length,
+				multicastAddress, multicastPort);
+		
+		try {
+			multicastSocket.setLoopbackMode(true);
+		} catch (SocketException e1) {
+			e1.printStackTrace();
+		}
+
+		try {
+			multicastSocket.send(packet);
+		} catch (IOException e) {
+			System.err.println("Failed to send packet");
+			e.printStackTrace();
+			return false;
+		}
+
+		return true;
+	}
+
+	public byte[] receiveMessage() throws Exception {
+
+		if (multicastSocket == null) {
+			throw new Exception();
+		}
+
+		byte[] buffer = new byte[MAX_PACKET_SIZE];
+
+		DatagramPacket packet = new DatagramPacket(buffer, MAX_PACKET_SIZE);
+
+		try {
+			multicastSocket.receive(packet);
+		} catch (IOException e) {
+			System.err.println("Failed to send packet");
+			e.printStackTrace();
+			return null;
+		}
+		
+		byte[] message = new byte[packet.getLength()];
+		
+		System.arraycopy(buffer, 0, message, 0, packet.getLength());
+
+		return message;
 	}
 
 }
